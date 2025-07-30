4332e16c3b88d72e58f75fbde02eaad364d82db2
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-07-30 22:38:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotating an Array/rotating-an-array.cpp
@@ -0,0 +1,22 @@
+// User function template for C++
+
+class Solution {
+  public:
+    void leftRotate(vector<int>& arr, int d) {
+        // code here
+         vector<int> vec;
+        int n=arr.size();
+        for(int i=d;i<n;i++)
+        {
+            vec.push_back(arr[i]);
+        }
+        for(int i=0;i<d;i++)
+        {
+            vec.push_back(arr[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=vec[i];
+        }
+    }
+};
