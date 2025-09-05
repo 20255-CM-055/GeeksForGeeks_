852dae23153e48a7671723b19aff94152fb95cb4
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-09-05 09:42:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -0,0 +1,44 @@
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+Node* convert(vector<int> &arr,int n)
+{
+    Node* head=new Node(arr[0]);
+    Node* temp=head;
+    
+    for(int i=1;i<n;i++)
+    {
+        temp->next=new Node(arr[i]);
+        temp=temp->next;
+    }
+    
+    return head;
+}
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        vector<int> arr;
+        Node* temp=head;
+        
+        while(temp!=NULL)
+        {
+            arr.push_back(temp->data);
+            temp=temp->next;
+        }
+        
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        
+        return convert(arr,n);
+    }
+};
