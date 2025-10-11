ffb69eb3a57135070f4f8bc1ffcd4e8a74d9cc39
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-10-11 23:24:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 2/for-loop-2.cpp
@@ -0,0 +1,17 @@
+void utility(string s) {
+    // length of string is given by s.length()
+    // character at any index i is given by s[i]
+
+    // Write your code here
+    // string ans="";
+    int n=s.size();
+    
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+           cout<<s[i];
+        }
+    }
+    
+    // s=ans;
+    
+}
