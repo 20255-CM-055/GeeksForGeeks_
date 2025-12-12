d32087840cca306447ebe0ab642aac457b77a42d
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2025-12-12 06:12:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
@@ -2,16 +2,12 @@ class Solution {
   public:
     vector<vector<int>> transpose(vector<vector<int>>& mat) {
         // code here
-       
         int r=mat.size();
         int c=mat[0].size();
-        
         vector<vector<int>> ans(c,vector<int>(r));
         
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 ans[j][i]=mat[i][j];
             }
         }
