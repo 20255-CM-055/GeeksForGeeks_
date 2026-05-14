2c6fab4b011695694ce72b21900be03ff67f7e8d
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2026-05-14 10:05:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/First Occurence/first-occurence.cpp
+++ Difficulty: Basic/First Occurence/first-occurence.cpp
@@ -2,7 +2,12 @@ class Solution {
   public:
     int firstOccurence(string& txt, string& pat) {
         // code here
-        int index=txt.find(pat);
-        return index;
+        int ans=-1;
+        
+        // for(int i=0;i<txt.size();i++){
+           ans=txt.find(pat);
+        // }
+        
+        return ans;
     }
 };
