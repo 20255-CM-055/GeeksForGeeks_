7efc76234452404e6deba138da651469493baa42
Tallapudi Nandini Satya <119557891+20255-CM-055@users.noreply.github.com>
2026-05-17 15:02:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Spaces/remove-spaces.cpp
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -2,17 +2,17 @@ class Solution {
   public:
     string removeSpaces(string& s) {
         // code here
-        string ans;
-        
-        for(char ch:s){
-            if(ch==' '){
-                continue;
-            }
-            else{
-                ans=ans+ch;
-            }
-        }
-        
-        return ans;
+       string ans;
+       
+       for(char ch:s){
+           if(ch==' '){
+               continue;
+           }
+           else{
+               ans=ans+ch;
+           }
+       }
+       
+       return ans;
     }
 };
